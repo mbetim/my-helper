807a4bd5f4b28a8935a18c1207a92d5b88829e67
mbetim <ma.betim@hotmail.com>
2021-01-06 15:00:24 -0300

    Create option to add code projects

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,52 @@
-import { app, Menu, shell, Tray } from "electron";
-import path from "path";
+import { app, dialog, Menu, shell, Tray } from "electron";
+import ElectronStore from "electron-store";
+import Path from "path";
+import { spawn } from "child_process";
+
+interface Store {
+  codeProjects: { name: string; path: string }[];
+}
 
 let tray: Tray | null = null;
+const store = new ElectronStore<Store>({
+  schema: {
+    codeProjects: { type: "array", default: [] },
+  },
+});
 
 const openLink = async (link: string) => shell.openExternal(link);
 
-const createTray = () => {
-  console.log("Ready");
-  tray = new Tray(path.resolve(__dirname, "assets", "icon.png"));
+const addCodeProject = () => {
+  const resultDialog = dialog.showOpenDialogSync({ properties: ["openDirectory"] });
+
+  if (!resultDialog) return;
+
+  const [path] = resultDialog;
+  const name = Path.basename(path);
+  const oldProjects = store.get("codeProjects");
+
+  store.set("codeProjects", [...oldProjects, { name, path }]);
+  renderTray();
+};
+
+const openCodeProject = (path: string) => spawn("code", [path], { shell: true });
+
+const removeCodeProject = (index: number) => {
+  const oldProjects = store.get("codeProjects");
+
+  store.set(
+    "codeProjects",
+    oldProjects.filter((_, i) => i !== index)
+  );
+
+  renderTray();
+};
+
+const renderTray = () => {
+  if (!tray) return;
+  console.log("Rendering Tray");
+
+  const codeProjects = store.get("codeProjects");
 
   const contextMenu = Menu.buildFromTemplate([
     {
@@ -18,6 +57,15 @@ const createTray = () => {
       ],
     },
     { type: "separator" },
+    ...codeProjects.map(({ name, path }, i) => ({
+      label: name,
+      submenu: [
+        { label: "Open", click: () => openCodeProject(path) },
+        { label: "Remove", click: () => removeCodeProject(i) },
+      ],
+    })),
+    { label: "Add a project", click: addCodeProject },
+    { type: "separator" },
     { role: "quit" },
   ]);
 
@@ -25,4 +73,9 @@ const createTray = () => {
   tray.setContextMenu(contextMenu);
 };
 
-app.on("ready", createTray);
+app.on("ready", () => {
+  console.log("App ready");
+
+  tray = new Tray(Path.resolve(__dirname, "assets", "icon.png"));
+  renderTray();
+});
